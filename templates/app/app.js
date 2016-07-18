76aa0caa28aaa5a5e5624d847a2712e105d73239
vivalalova <vivalalova0@gmail.com>
2016-07-18 18:27:26 +0800

    (╬ ಠ 益ಠ) middleware

--- templates/app/app.js
+++ templates/app/app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
-const cookieParser = require('cookie-parser')
 const session = require('./api/config/session.js')
-const bodyParser = require('body-parser')
 const env = require('./api/config/env.js')
 const app = express()
 
@@ -18,13 +16,10 @@ if (env.isDevelopment === true) {
     app.use(logger('dev'))
 }
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.use(session)
 
+require('./api/config/middleware.js')(app)
 require('./api/config/route.js')(app)
 
 
