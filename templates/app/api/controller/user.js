432b4e3ca5d6dc9639cdcdea271897d4e6732fb2
Lova <vivalalova0@gmail.com>
2017-02-24 04:51:20 +0800

    migrate for node 7.6

--- templates/app/api/controller/user.js
+++ templates/app/api/controller/user.js
@@ -5,63 +5,60 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 const crypto = require('crypto')
 
-const { wrap: async } = require('co')
-
 module.exports = {
-    find: async(function*(req, res, next) {
+    find: async function (req, res, next) {
         try {
-            const result = yield User.find().skip(req.query.skip).limit(req.query.limit).exec()
+            const result = await User.find().skip(req.query.skip).limit(req.query.limit).exec()
             return res.send(result)
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    findOne: async(function*(req, res, next) {
-        const result = yield User.findOne({ _id: req.params.id })
+    },
+    findOne: async function (req, res, next) {
+        const result = await User.findOne({ _id: req.params.id })
         return res.send(result)
-    }),
-    create: async(function*(req, res) {
+    },
+    create: async function (req, res) {
         try {
             const user = new User(req.body)
-            const result = yield user.save()
+            const result = await user.save()
             return res.send(result)
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    update: async(function*(req, res, next) {
+    },
+    update: async function (req, res, next) {
         try {
-            const result = yield User.update({ _id: req.params.id }, req.body)
-            helper.errHandler(result, res, function() {
+            const result = await User.update({ _id: req.params.id }, req.body)
+            helper.errHandler(result, res, function () {
                 return res.send(result)
             })
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    delete: async(function*(req, res, next) {
+    },
+    delete: async function (req, res, next) {
         try {
-            const result = yield User.remove({ _id: req.params.id })
-            helper.errHandler(result, res, function() {
+            const result = await User.remove({ _id: req.params.id })
+            helper.errHandler(result, res, function () {
                 return res.send(result)
             })
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    login: async(function*(req, res, next) {
+    },
+    login: async function (req, res, next) {
         try {
-            const user = yield User.findOne({ account: req.params.account })
+            const user = await User.findOne({ account: req.params.account })
 
-            helper.errHandler(user, res, function(a, b) {
+            helper.errHandler(user, res, function (a, b) {
                 // body...
             })
-
 
             res.send(user)
         } catch (err) {
             console.log(err)
             return helper.err(err, res)
         }
-    }),
+    }
 }
--- templates/controller.js
+++ templates/controller.js
@@ -4,45 +4,45 @@ const helper = require('../helper/helper.js')
 const { wrap: async } = require('co')
 
 module.exports = {
-    find: async(function*(req, res, next) {
+    find: async function (req, res, next) {
         try {
-            const result = yield {Name}.find().skip(req.query.skip).limit(req.query.limit).exec()
+            const result = await {Name}.find().skip(req.query.skip).limit(req.query.limit).exec()
             return res.send(result)
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    findOne: async(function*(req, res, next) {
-        const result = yield {Name}.findOne({ _id: req.params.id })
+    },
+    findOne: async function (req, res, next) {
+        const result = await {Name}.findOne({ _id: req.params.id })
         return res.send(result)
-    }),
-    create: async(function*(req, res) {
+    },
+    create: async function (req, res) {
         try {
             const {name} = new {Name}(req.body)
-            const result = yield {name}.save()
+            const result = await {name}.save()
             return res.send(result)
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    update: async(function*(req, res, next) {
+    },
+    update: async function (req, res, next) {
         try {
-            const result = yield {Name}.update({ _id: req.params.id }, req.body)
+            const result = await {Name}.update({ _id: req.params.id }, req.body)
             helper.errHandler(result, res, function() {
                 return res.send(result)
             })
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
-    delete: async(function*(req, res, next) {
+    },
+    delete: async function (req, res, next) {
         try {
-            const result = yield {Name}.remove({ _id: req.params.id })
+            const result = await {Name}.remove({ _id: req.params.id })
             helper.errHandler(result, res, function() {
                 return res.send(result)
             })
         } catch (err) {
             return helper.err(err, res)
         }
-    }),
+    },
 }
